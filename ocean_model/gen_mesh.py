cecdea75632d1cbf59192e194062cdc30ba45f27
Evan Cummings <cummings.evan@gmail.com>
2017-10-20 16:37:31 +0200

    really nice ocean mesh

--- ocean_model/gen_mesh.py
+++ ocean_model/gen_mesh.py
@@ -9,7 +9,7 @@ mask = data['amask'][:]
 lat  = data['lat'][:]
 lon  = data['lon'][:]
 
-# reduce the size of the dataset for easy plotting :
+# reduce the size of the dataset by 50% for easy plotting :
 mask = np.delete(mask, list(range(0, mask.shape[0], 2)), axis=0)
 mask = np.delete(mask, list(range(0, mask.shape[1], 2)), axis=1)
 
@@ -37,10 +37,11 @@ p    = Proj(txt)
 # convert to projection coordinates :
 X,Y = p(LON, LAT)
 
-msh_name = 'crude_mesh'                   # name of all outputs
-out_dir  = 'meshes/'                      # directory for all outputs
-S        = mask                           # mathematical function to contour
-S[S>0.1] = 1.0
+msh_name = 'crude_mesh'      # name of all outputs
+out_dir  = 'meshes/'         # directory for all outputs
+S        = mask              # mathematical function to contour
+S[S>0.1] = 1.0               # no shelves, etc.
+skip_pts = 1                 # number of contour points to skip
 
 # the MeshGenerator instance initialized with the matrix coordinates,
 # output file name, and output directory :
@@ -51,11 +52,12 @@ m = MeshGenerator(X, Y, msh_name, out_dir)
 # contour is desired.  Parameter "distance_check" is the distance from each node
 # to check for edge intersections which may arise from removing points; 
 # increase the distance if overlaps occur :
-m.create_contour(S, zero_cntr=0.1, skip_pts=1,
-                 distance_check=50, min_nodes=25)
+m.create_contour(S, zero_cntr=0.1, skip_pts=skip_pts,
+                 distance_check=50, min_nodes=5)
 
-# if land masses overlap, combine them :
-m.unify_overlapping_contours()
+# if points have been removed, land masses may overlap and therefore must be
+# combined :
+if skip_pts > 0:  m.unify_overlapping_contours()
 
 # plot the resulting contours :
 m.plot_contour(legend=False)
@@ -63,23 +65,23 @@ m.plot_contour(legend=False)
 # write the gmsh contour to the "msh_name".geo file with characteristic 
 # cell diameter "lc".  If "boundary_extend"=True, the edge size of the contour 
 # is extrapolated into the interior :
-m.write_gmsh_contour(lc=100000, boundary_extend=False)
+m.write_gmsh_contour(lc=10000000, boundary_extend=True)#False)
 
 # get the number of contours for the mesh :
 num_ctrs = m.num_contours()
-
-# add identical edge attractors to all contours with sequential identifier.
-# the parameter "NNodesByEdge" adds extra nodes to the edge for the purpose
-# of refinement, increase this if your edges are much longer than your 
-# cell refinement "lcMin" set by m.add_threshold() below :
-for i in range(num_ctrs):
-  m.add_edge_attractor(field=i, contour_index=i, NNodesByEdge=3)
-
-# for each edge attractor, add a threshold for mesh refinement in the vicinity 
-# of the edge :
-for i in range(num_ctrs):
-  # parameters are respectively: field, ifield, lcMin, lcMax, distMin, distMax
-  m.add_threshold(num_ctrs+i, i, 100000, 10000000, 0, 20000000)
+#
+## add identical edge attractors to all contours with sequential identifier.
+## the parameter "NNodesByEdge" adds extra nodes to the edge for the purpose
+## of refinement, increase this if your edges are much longer than your 
+## cell refinement "lcMin" set by m.add_threshold() below :
+#for i in range(num_ctrs):
+#  m.add_edge_attractor(field=i, contour_index=i, NNodesByEdge=3)
+#
+## for each edge attractor, add a threshold for mesh refinement in the vicinity 
+## of the edge :
+#for i in range(num_ctrs):
+#  # parameters are respectively: field, ifield, lcMin, lcMax, distMin, distMax
+#  m.add_threshold(num_ctrs+i, i, 1000000, 10000000, 0, 20000000)
 
 # finialize the creation of the "msh_name".geo file :
 m.finish()
